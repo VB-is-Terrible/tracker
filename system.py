028078d6ca3dc18dd1458a09c80e4907edb438f7
William Chow <431741580derp@gmail.com>
2018-06-25 22:21:43 +1000

    Add type to json of System

--- system.py
+++ system.py
@@ -3,3 +3,7 @@ from common import JSONable
 class System(JSONable):
 	def __init__(self):
 		self.projects = []
+	def json(self):
+		result = super().json()
+		result['type'] = 'System'
+		return result
