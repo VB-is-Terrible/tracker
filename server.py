4b896dc19fa6f09a9190885287151a5c1d1a6a6a
William Chow <431741580derp@gmail.com>
2018-06-25 19:55:02 +1000

    Add desc to yaml

--- server.py
+++ server.py
@@ -30,16 +30,3 @@ def save():
 app = Flask(__name__)
 app.secret_key = b"\xc5S\xbe\xfa\xdf\xd5\x80I\xdfi\xa5[h\xda\xb8\xacb\xd2\xe2\x83Kc\x89]\xb2\xed\xbd\x8cc pM1(YC\x96\xf2Y+\xfd\x16\x99A\xc6\x80\xa2\x08\xe6\xa3\xfb\x17|\xf9\x82H\xb6\xb41\x8d\x13\x12B\xa6\xef\xab\xe7'8\x973\xec\x964jr4\xe5U\xf7\xe4|J\x88\xf3K\x96\xa2E6\x94\x85\x85\xc9\xf8\x07\xb0\xc3\xc1\xb4"
 load()
-
-def update():
-        global data
-        for proj in data.projects:
-                proj.meta = 0
-                proj.desc = ''
-        for super in data.superprojects:
-                proj = project.Project(super.name, id=super.id)
-                proj.dependencies = super.projects
-                proj.meta = 1
-                data.projects.append(proj)
-        del data.superprojects
-        save()
