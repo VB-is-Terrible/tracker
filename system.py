9c151ed3210b568e9e474c76f2ec1f787dd85139
William Chow <431741580derp@gmail.com>
2018-06-26 15:29:48 +1000

    Update system

--- system.py
+++ system.py
@@ -2,8 +2,10 @@ from common import JSONable
 
 class System(JSONable):
 	def __init__(self):
-		self.projects = []
+		self.projects = {}
 	def json(self):
 		result = super().json()
 		result['type'] = 'System'
 		return result
+	def get_event_by_id(self, id):
+		return self.projects[id]
