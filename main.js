bb0cb86976b19c03beab8a3f5fb80cd1bc4dbd4c
William Chow <431741580derp@gmail.com>
2018-06-29 16:19:31 +1000

    Put projects inside a div

--- main.js
+++ main.js
@@ -8,10 +8,11 @@ const main = async () => {
 	await Elements.get('projects-Project');
 	let json = await request;
 	data = Projects.System.fromJSON(json);
+	let projects = document.querySelector('#projects');
 	for (let project_id of data.projects.keys()) {
 		let display = document.createElement('elements-projects-project-display');
 		display.data = data.get_event_by_id(project_id);
-		document.body.append(display);
+		projects.append(display);
 	}
 	console.log('done');
 }
