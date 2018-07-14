a416a9ea03cc32d0ca7e4be5f055141014fdf4eb
William Chow <431741580derp@gmail.com>
2018-07-15 03:11:59 +1000

    Made updater

--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, Response
 from flask import render_template, request, redirect, url_for, abort, send_from_directory, jsonify
 import json, yaml
-import project
+import project as Project
 from server import app, load, save, data
 
 @app.route('/')
@@ -11,7 +11,12 @@ def index():
 
 @app.route('/start')
 def start():
-        return json.dumps(data, cls=project.ProjectEncoder)
+        return json.dumps(data, cls=Project.ProjectEncoder)
+
+@app.route('/create', methods=['POST'])
+def project_create():
+        data.add_project_from_request(request)
+        return json.dumps(data.new_patch(), cls=Project.ProjectEncoder)
 
 @app.route('/elements/<path:path>')
 def elements(path):
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import project
 import os
 import subprocess
 from flask import Flask
-
+from updater import update
 PROJECT = 'persist.yaml'
 DATA = 'projects2.yaml'
 
@@ -33,13 +33,5 @@ app = Flask(__name__)
 app.secret_key = b"\xc5S\xbe\xfa\xdf\xd5\x80I\xdfi\xa5[h\xda\xb8\xacb\xd2\xe2\x83Kc\x89]\xb2\xed\xbd\x8cc pM1(YC\x96\xf2Y+\xfd\x16\x99A\xc6\x80\xa2\x08\xe6\xa3\xfb\x17|\xf9\x82H\xb6\xb41\x8d\x13\x12B\xa6\xef\xab\xe7'8\x973\xec\x964jr4\xe5U\xf7\xe4|J\x88\xf3K\x96\xa2E6\x94\x85\x85\xc9\xf8\x07\xb0\xc3\xc1\xb4"
 load()
 
-def update():
-        if project_persist['update_version'] == 0:
-                projs = {}
-                for proj in data.projects:
-                        projs[proj.id] = proj
-                        data.projects = projs
-                project_persist['update_version'] = 1
-                save()
-
-update()
+if update(project_persist, data):
+        save()
