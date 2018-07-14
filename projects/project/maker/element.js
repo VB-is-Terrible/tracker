e3f049f7e4f7046b5ec8fc465d5a24e1fb9a02e1
William Chow <431741580derp@gmail.com>
2018-07-15 02:36:00 +1000

    Make project-maker iwith a lot of todos

--- projects/project/maker/element.js
+++ projects/project/maker/element.js
@@ -1,16 +1,16 @@
 'use strict';
 
-Elements.get();
+Elements.get('drag-element', 'projects-Project');
 {
 const main = async () => {
 
-await Elements.get();
+await Elements.get('drag-element');
 /**
  * [ProjectsProjectMaker Description]
  * @augments Elements.elements.backbone2
  * @type {Object}
  */
-Elements.elements.ProjectsProjectMaker = class ProjectsProjectMaker extends Elements.elements.backbone2 {
+Elements.elements.ProjectsProjectMaker = class ProjectsProjectMaker extends Elements.elements.dragged2 {
 	constructor () {
 		super();
 		const self = this;
@@ -18,9 +18,66 @@ Elements.elements.ProjectsProjectMaker = class ProjectsProjectMaker extends Elem
 		this.name = 'ProjectsProjectMaker';
 		const shadow = this.attachShadow({mode: 'open'});
 		let template = Elements.importTemplate(this.name);
-
+		let done = template.querySelector('#Done');
+		done.addEventListener('click', (e) => {
+			self.create();
+		});
+		let cancel = template.querySelector('#Cancel');
+		cancel.addEventListener('click', (e) => {
+			self.clear();
+		});
+		for (let input of template.querySelectorAll('input')) {
+			if (input.type === 'text') {
+				input.addEventListener('mousedown', (e) => {
+					e.stopPropagation();
+				});
+			}
+		}
+		let progress = template.querySelector('#AnsProgress');
+		let progressAmount = template.querySelector('#AnsProgressAmount');
+		progress.addEventListener('change', (e) => {
+			progressAmount.disabled = !progress.checked;
+		})
 		//Fancy code goes here
 		shadow.appendChild(template);
+		this.applyPriorProperty('database', null);
+	}
+	create () {
+		if (!Elements.loadedElements.has('projects-Project')) {
+			console.log('Core js has not loaded');
+			return;
+		}
+		let name = this.shadowRoot.querySelector('#AnsName').value;
+		let desc = this.shadowRoot.querySelector('#AnsDesc').value;
+		let progress = this.shadowRoot.querySelector('#AnsProgress').checked;
+		let progressAmount = this.shadowRoot.querySelector('#AnsProgressAmount').value;
+		let meta = this.shadowRoot.querySelector('#AnsMeta').checked;
+		let project = new Projects.Project(this.database, name, null, desc, progress ? progressAmount : undefined);
+		// TODO: Add feedback
+		if (name === '') {return;}
+		project.meta = meta ? 1 : 0;
+		// TODO: Add dependencies
+		this.send_create(project);
+	}
+	async send_create (project) {
+		// this.hideWindow();
+		this.clear();
+		let result = await this.database.add_project(project);
+		if (!result) {
+			// TODO: Add notification
+		}
+	}
+	clear () {
+		let name = this.shadowRoot.querySelector('#AnsName');
+		let desc = this.shadowRoot.querySelector('#AnsDesc');
+		let progress = this.shadowRoot.querySelector('#AnsProgress');
+		let progressAmount = this.shadowRoot.querySelector('#AnsProgressAmount');
+		let meta = this.shadowRoot.querySelector('#AnsMeta');
+		name.value = '';
+		desc.value = '';
+		progress.checked = false;
+		progressAmount.value = null;
+		meta.checked = false;
 	}
 };
 
