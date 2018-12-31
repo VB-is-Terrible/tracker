4e0c88b6151eae2fad3e7a764227db7589f4fcd4
William Chow <431741580derp@gmail.com>
2018-12-31 22:42:03 +1100

    Changed global Data -> DATA

--- main.js
+++ main.js
@@ -2,17 +2,17 @@
 
 Elements.get('projects-Project', 'projects-project-display',
              'drag-body', 'drag-element', 'projects-project-maker');
-let Data;
+let DATA;
 {
 const main = async () => {
 	let request = Elements.request('/start');
 	await Elements.get('projects-Project');
 	let json = await request;
-	Data = Projects.System.fromJSON(json);
+	DATA = Projects.System.fromJSON(json);
 	let projects = document.querySelector('#projects');
-	for (let project_id of Data.projects.keys()) {
+	for (let project_id of DATA.projects.keys()) {
 		let display = document.createElement('elements-projects-project-display');
-		display.data = Data.get_event_by_id(project_id);
+		display.data = DATA.get_event_by_id(project_id);
 		projects.append(display);
 	}
 	console.log('done');
