07b17c61273fe67649e3a0743765777ccbbf5711
William Chow <431741580derp@gmail.com>
2018-12-31 20:42:06 +1100

    Changed global name

--- main.js
+++ main.js
@@ -2,20 +2,19 @@
 
 Elements.get('projects-Project', 'projects-project-display',
              'drag-body', 'drag-element', 'projects-project-maker');
-let data;
+let Data;
 {
 const main = async () => {
 	let request = Elements.request('/start');
 	await Elements.get('projects-Project');
 	let json = await request;
-	data = Projects.System.fromJSON(json);
+	Data = Projects.System.fromJSON(json);
 	let projects = document.querySelector('#projects');
-	for (let project_id of data.projects.keys()) {
+	for (let project_id of Data.projects.keys()) {
 		let display = document.createElement('elements-projects-project-display');
-		display.data = data.get_event_by_id(project_id);
+		display.data = Data.get_event_by_id(project_id);
 		projects.append(display);
 	}
-        document.querySelector('elements-projects-project-maker').database = data;
 	console.log('done');
 }
 
