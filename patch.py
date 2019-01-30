f40daa619e34c1d65c903e1315f3e4202ec9ffa0
William Chow <431741580derp@gmail.com>
2019-01-30 19:48:37 +1100

    Add previous and current version tags to patches

--- patch.py
+++ patch.py
@@ -1,8 +1,10 @@
 from common import JSONable
 
+
 class Patch(JSONable):
-        def __init__(self):
+        def __init__(self, previous: int, current: int):
                 self.create = []
                 self.update = []
                 self.delete = []
-        
+                self.previous = previous
+                self.current = current
