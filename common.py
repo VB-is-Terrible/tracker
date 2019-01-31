21ca3176d10d02ec2ea81d56f38314ff5edca4b6
William Chow <431741580derp@gmail.com>
2019-01-31 14:37:21 +1100

    Improved readability

--- common.py
+++ common.py
@@ -1,12 +1,19 @@
+def valid_property(object, property: str):
+        if property.startswith('_'):
+                return False
+        if callable(getattr(object, property)):
+                return False
+        return True
+
+
 class JSONable():
         def json(self, excludes = set()):
                 result = {}
                 class_dir = dir(type(self))
-                props = [x for x in dir(self) if
-                         not x.startswith('_') and
-                         not callable(getattr(self, x)) and
-                         x not in class_dir and
-                         x not in excludes]
+                props = [x for x in dir(self)
+                         if valid_property(self, x)
+                         and x not in class_dir
+                         and x not in excludes]
                 for prop in props:
                         result[prop] = self.__getattribute__(prop)
                 return result
