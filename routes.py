f4c88aa0250487d59c330a3c3f24d88983f4d466
William Chow <431741580derp@gmail.com>
2019-01-31 14:38:55 +1100

    Fixed warning in routes

--- routes.py
+++ routes.py
@@ -1,17 +1,19 @@
-from flask import Flask, Response
-from flask import render_template, request, redirect, url_for, abort, send_from_directory, jsonify
-import json, yaml
+from flask import request, send_from_directory
+import json
 import project as Project
-from server import app, load, save, data
+from server import app, data
+
 
 @app.route('/')
 def index():
         return send_from_directory('./', 'projects.html')
         # return send_from_directory('./main.html')
 
+
 @app.route('/start')
 def start():
         return json.dumps(data, cls=Project.ProjectEncoder)
+
 
 @app.route('/create', methods=['POST'])
 def project_create():
