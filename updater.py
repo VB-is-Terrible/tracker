27f00716a76e77dd84edd41c41ca2a06f22dc3c7
William Chow <431741580derp@gmail.com>
2019-02-04 18:22:57 +1100

    Wrote update function for patch holder

--- updater.py
+++ updater.py
@@ -3,6 +3,7 @@ import importlib
 updaters = {}
 LAST_VERSION = 3
 
+
 def update0(project_persist, data):
 	if project_persist['update_version'] == 0:
 		projs = {}
@@ -11,7 +12,9 @@ def update0(project_persist, data):
 			data.projects = projs
 		project_persist['update_version'] = 1
 
+
 updaters[0] = update0
+
 
 def update1(persist, data):
 	if persist['update_version'] == 1:
@@ -19,7 +22,21 @@ def update1(persist, data):
 		data.patch = patch.Patch()
 		persist['update_version'] = 2
 
+
 updaters[1] = update1
+
+
+def update2(persist, data):
+	if persist['update_version'] == 2:
+		data.version = 0
+		del data.patch
+		patches = importlib.import_module('patch_holder')
+		data.patches = patches.PatchHolder(0)
+		persist['update_version'] = 3
+
+
+updaters[2] = update2
+
 
 def update(persist, data):
 	run = False
