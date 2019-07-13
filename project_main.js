ca072d08b40c945f3cba798c258a944b444bc2d4
William Chow <431741580derp@gmail.com>
2019-07-14 00:24:08 +1000

    Added stuff to display detailed project information

---
+++ project_main.js
@@ -0,0 +1,26 @@
+'use strict'
+
+Elements.get('projects-Project', 'projects-project-full');
+
+let DATA;
+{
+const get_project_id = () => {
+	let page_locations = window.location.pathname.split('/');
+	return parseInt(page_locations[page_locations.length - 1]);
+}
+
+const main = async () => {
+	let request = Elements.request('../start');
+	await Elements.get('projects-Project');
+	let json = await request;
+	DATA = Projects.System.fromJSON(json);
+	let projects = document.querySelector('#a');
+	let page_id = get_project_id();
+	const current_project = DATA.get_event_by_id(page_id);
+	projects.data = current_project;
+	document.querySelector('title').innerHTML = current_project.name;
+	console.log('done');
+};
+
+main();
+}
