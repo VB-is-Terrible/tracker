773498458647f70a8f9c84dd5e23b4842f102edf
William Chow <431741580derp@gmail.com>
2019-09-29 23:19:01 +1000

    Move DATA global into Projects namespace

--- main.js
+++ main.js
@@ -3,17 +3,16 @@
 Elements.get('projects-Project', 'projects-project-display',
              'drag-body', 'drag-element', 'projects-project-maker', 'projects-project-editor');
 
-let DATA;
 {
 const main = async () => {
 	let request = Elements.request('/start');
 	await Elements.get('projects-Project', 'drag-element');
 	let json = await request;
-	DATA = Projects.System.fromJSON(json);
+	Projects.main_project = Projects.System.fromJSON(json);
 	let projects = document.querySelector('#projects');
-	for (let project_id of DATA.projects.keys()) {
+	for (let project_id of Projects.main_project.projects.keys()) {
 		let display = document.createElement('elements-projects-project-display');
-		display.data = DATA.get_event_by_id(project_id);
+		display.data = Projects.main_project.get_event_by_id(project_id);
 		projects.append(display);
 	}
 	console.log('done');
--- project_main.js
+++ project_main.js
@@ -2,7 +2,6 @@
 
 Elements.get('projects-Project', 'projects-project-full');
 
-let DATA;
 {
 const get_project_id = () => {
 	let page_locations = window.location.pathname.split('/');
@@ -13,10 +12,10 @@ const main = async () => {
 	let request = Elements.request('../start');
 	await Elements.get('projects-Project');
 	let json = await request;
-	DATA = Projects.System.fromJSON(json);
+	Projects.main_project = Projects.System.fromJSON(json);
 	let projects = document.querySelector('#a');
 	let page_id = get_project_id();
-	const current_project = DATA.get_event_by_id(page_id);
+	const current_project = Projects.main_project.get_event_by_id(page_id);
 	projects.data = current_project;
 	document.querySelector('title').innerHTML = current_project.name;
 	console.log('done');
