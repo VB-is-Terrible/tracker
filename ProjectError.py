14e7754467c920ac0367710ea29cc540d56df9c6
William Chow <431741580derp@gmail.com>
2019-09-30 23:05:25 +1000

    More error types

--- ProjectError.py
+++ ProjectError.py
@@ -3,4 +3,28 @@ class InputException(Exception):
 
 
 class InvalidIdException(InputException):
-        pass
+        message = 'Invalid id: <{}>'
+
+        def __init__(self, id: int):
+                super(self.message.format(id))
+
+
+class InvalidTypeException(InputException):
+        message = 'Invalid type for field <{}> in <{}>, expected <{}>'
+
+        def __init__(self, missing_field: str, dict_name: str, expected):
+                super(self.message.format(missing_field, dict_name, expected))
+
+
+class MissingFieldException(InputException):
+        message = 'Could not find field: <{}> in <{}>'
+
+        def __init__(self, missing_field: str, dict_name: str):
+                super(self.message.format(missing_field, dict_name))
+
+
+class InvalidProgressException(InputException):
+        message = 'Field <{}> has invalid value <{}>'
+
+        def __init__(self, invalid_value, field):
+                super(self.message.format(field, invalid_value))
