337ee2108067c90b10e564f61954311334469c9a
William Chow <431741580derp@gmail.com>
2019-10-01 01:55:59 +1000

    Fix init calls

--- ProjectError.py
+++ ProjectError.py
@@ -6,26 +6,27 @@ class InvalidIdException(InputException):
         message = 'Invalid id: <{}>'
 
         def __init__(self, id: int):
-                super(self.message.format(id))
+                super().__init__(self.message.format(id))
 
 
 class InvalidTypeException(InputException):
-        message = 'Invalid type {} for field {} in {}, expected {}'
+        message = 'Invalid type {} for field \'{}\' in {}, expected {}'
 
-        def __init__(self, bad_type, missing_field: str, dict_name: str, expected):
-                super(self.message.format(bad_type, missing_field,
-                                          dict_name, expected))
+        def __init__(self, bad_type, missing_field: str, dict_name: str,
+                     expected):
+                super().__init__(self.message.format(bad_type, missing_field,
+                                                     dict_name, expected))
 
 
 class MissingFieldException(InputException):
-        message = 'Could not find field: <{}> in <{}>'
+        message = 'Could not find field: <{}> in \'{}\''
 
         def __init__(self, missing_field: str, dict_name: str):
-                super(self.message.format(missing_field, dict_name))
+                super().__init__(self.message.format(missing_field, dict_name))
 
 
 class InvalidProgressException(InputException):
-        message = 'Field <{}> has invalid value <{}>'
+        message = 'Field \'{}\' has invalid value <{}>'
 
         def __init__(self, invalid_value, field):
-                super(self.message.format(field, invalid_value))
+                super().__init__(self.message.format(field, invalid_value))
