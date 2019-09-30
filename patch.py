876919d7169839c390f6ab704565d4b15c8e0486
William Chow <431741580derp@gmail.com>
2019-10-01 01:07:41 +1000

    Add add_change to Patch

--- patch.py
+++ patch.py
@@ -1,12 +1,13 @@
 from common import JSONable
 import json
 import project as Project
+from ChangeSet import ChangeSet
 
 
 class Patch(JSONable):
         def __init__(self, previous: int, current: int):
                 self.create = []
-                self.update = []
+                self.change = []
                 self.delete = []
                 self.previous = previous
                 self.current = current
@@ -18,3 +19,7 @@ class Patch(JSONable):
         def add_create(self, project: Project.Project):
                 string = json.dumps(project, cls=Project.ProjectEncoder)
                 self.create.append(string)
+
+        def add_change(self, change_set: ChangeSet):
+                string = json.dumps(change_set, cls=Project.ProjectEncoder)
+                self.change.append(string)
